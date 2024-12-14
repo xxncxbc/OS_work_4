81a91cd5b4413710558dd4914a1f69e9c1de0dfc
xxncxbc <674holland@gmail.com>
2024-12-14 15:48:36 +0300

    Добавьте файлы проекта.

---
+++ OS_work_4.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <vector>
+using namespace std;
+
+int main(){
+    //инициализируем обьекты синхронизации
+    HANDLE EventA = CreateEvent(NULL, FALSE, FALSE, TEXT("EventA"));
+    HANDLE EventB = CreateEvent(NULL, FALSE, FALSE, TEXT("EventB"));
+    HANDLE EventC = CreateEvent(NULL, FALSE, FALSE, TEXT("EventC"));
+    HANDLE EventD = CreateEvent(NULL, FALSE, FALSE, TEXT("EventD"));
+    HANDLE EndParent = CreateEvent(NULL, TRUE, FALSE, TEXT("EndParent"));
+    HANDLE EndChild = CreateEvent(NULL, TRUE, FALSE, TEXT("EndChild"));
+    HANDLE ParentSem = CreateSemaphore(NULL, 1, 1, TEXT("ParentSem"));
+    
+
+}
+
