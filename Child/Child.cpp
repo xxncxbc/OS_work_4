c709a5c78e27ba9e53124873adaefb3c9346a266
xxncxbc <99598045+xxncxbc@users.noreply.github.com>
2024-12-15 16:59:40 +0300

    added all cpp's

---
+++ Child/Child.cpp
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+vector<char> messages;
+int count_messages, delay;
+bool t = true;
+
+DWORD WINAPI Receive_f_p(LPVOID param) {
+    HANDLE EventA = OpenEvent(SYNCHRONIZE, FALSE, TEXT("EventA"));
+    HANDLE EventB = OpenEvent(SYNCHRONIZE, FALSE, TEXT("EventB"));
+    HANDLE events[] = { EventA, EventB };
+    if (!EventA || !EventB) { cerr << "Error"; return 0; }
+
+
+    while (t) {
+        DWORD waitCD = WaitForMultipleObjects(2, events, FALSE, 100);
+        if (waitCD == WAIT_OBJECT_0) {
+            messages.push_back('A');
+        }
+        else if (waitCD == WAIT_OBJECT_0 + 1) {
+            messages.push_back('B');
+        }
+    }
+
+    CloseHandle(EventA);
+    CloseHandle(EventB);
+    CloseHandle(events);
+
+    return 1;
+}
+
+int main(int argc, char* argv[]) {
+    //подключаем обьекты синхронизации
+    HANDLE EventC = OpenEvent(EVENT_MODIFY_STATE, FALSE, TEXT("EventC"));
+    HANDLE EventD = OpenEvent(EVENT_MODIFY_STATE, FALSE, TEXT("EventD"));
+    HANDLE ChildMutex = OpenMutex(MUTEX_ALL_ACCESS, FALSE, TEXT("ChildMutex"));
+    HANDLE EndChild = OpenEvent(EVENT_MODIFY_STATE, FALSE, TEXT("EndChild"));
+
+    WaitForSingleObject(ChildMutex, INFINITE); //подключаемся к мьютексу
+    cout << "I AM CHILD" << '\n';
+    HANDLE RecieveThread = CreateThread(NULL, 0, Receive_f_p, NULL, 0, NULL); // начинаем прием 
+
+    //устанавливаем задержку и количество сообщений
+    count_messages = atoi(argv[1]);
+    delay = atoi(argv[2]);
+
+
+
+    //генератор и отправка сообщений
+    srand(time(0));
+    int random_number;
+    for (int i = 0; i < count_messages; ++i) {
+        random_number = rand() % 2;
+        if (random_number == 0) { SetEvent(EventC); cout << "Sent: C" << '\n'; }
+        else { SetEvent(EventD); cout << "Sent: D" << '\n'; }
+        Sleep(delay);
+    }
+
+    t = false;
+
+    
+    
+    WaitForSingleObject(RecieveThread, INFINITE); //дожидаемся окончания приема
+    CloseHandle(RecieveThread); //закрываем поток приема
+    ReleaseMutex(ChildMutex); //отпускаем мьютекс
+    
+    //Выводим полученное
+    cout << "Received: " << '\n';
+    for (char k : messages) cout << k << '\n';
+    SetEvent(EndChild);
+    CloseHandle(ChildMutex);
+    CloseHandle(EventD);
+    CloseHandle(EventC);
+    CloseHandle(EndChild);
+
+    getchar();
+    return 0;
+}
+
+
---
+++ Parent/Parent.cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+vector<char> messages;
+int count_messages, delay;
+bool t = true;
+
+DWORD WINAPI Receive_f_ch(LPVOID param) {
+    HANDLE EventC = OpenEvent(SYNCHRONIZE, FALSE, TEXT("EventC"));
+    HANDLE EventD = OpenEvent(SYNCHRONIZE, FALSE, TEXT("EventD"));
+    HANDLE events[] = { EventC, EventD };
+    if (!EventC || !EventD) { cerr << "Error"; return 0; }
+    
+    
+    while (t) {
+        DWORD waitCD = WaitForMultipleObjects(2, events, FALSE, 100);
+        if (waitCD == WAIT_OBJECT_0) {
+            messages.push_back('C');
+        }
+        else if(waitCD == WAIT_OBJECT_0 + 1) {
+            messages.push_back('D');
+        }
+    }
+
+    CloseHandle(EventC);
+    CloseHandle(EventD);
+    CloseHandle(events);
+
+    return 1;
+}
+
+int main(int argc, char * argv[]) {
+    //подключаем обьекты синхронизации
+    HANDLE EventA = OpenEvent(EVENT_MODIFY_STATE, FALSE, TEXT("EventA"));
+    HANDLE EventB = OpenEvent(EVENT_MODIFY_STATE, FALSE, TEXT("EventB"));
+    HANDLE ParentSem = OpenSemaphore(SEMAPHORE_ALL_ACCESS, FALSE, TEXT("ParentSem"));
+    HANDLE EndParent = OpenEvent(EVENT_MODIFY_STATE, FALSE, TEXT("EndParent"));
+
+    WaitForSingleObject(ParentSem, INFINITE); //подключаемся к семафору
+
+    cout << "I AM PARENT" << '\n';
+    HANDLE RecieveThread = CreateThread(NULL, 0, Receive_f_ch, NULL, 0, NULL); // начинаем прием 
+
+    //устанавливаем задержку и количество сообщений
+    count_messages = atoi(argv[1]);
+    delay = atoi(argv[2]);
+
+   
+
+    //генератор и отправка сообщений
+    srand(time(0));
+    int random_number;
+    for (int i = 0; i < count_messages; ++i) {
+        random_number = rand() % 2;
+        if (random_number == 0) { SetEvent(EventA); cout << "Sent: A" << '\n'; }
+        else { SetEvent(EventB); cout << "Sent: B" << '\n'; }
+        Sleep(delay);
+    }
+
+    t = false;
+
+    
+
+    
+    WaitForSingleObject(RecieveThread, INFINITE); //дожидаемся окончания приема
+    ReleaseSemaphore(ParentSem, 1, NULL); //отпускаем семафор
+
+    //Выводим полученное
+    cout << "Received: " << '\n'; 
+    for (char k : messages) cout << k << '\n';
+
+    SetEvent(EndParent);
+
+    
+    CloseHandle(ParentSem);
+    CloseHandle(EventA);
+    CloseHandle(EventB);
+    CloseHandle(EndParent);
+    getchar();
+    return 0;
+}
+
+
